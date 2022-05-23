6284babff4b0624324f23ba932127a6da09e2708
Shusui MOYATANI <syusui.s@gmail.com>
2022-05-24 01:17:12 +0900

    update test

--- src/test.test.ts
+++ src/test.test.ts
@@ -1,6 +1,8 @@
 import assert from 'assert';
 import { describe } from 'mocha';
 
-describe('1 + 1 should be 2', function () {
-  assert(1 + 1 === 2);
+describe('1 + 1', function () {
+  it('should be 2', function () {
+    assert(1 + 1 === 2);
+  });
 });
