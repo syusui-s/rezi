26787211225e390824e3638c2509b5262d3e7216
Shusui MOYATANI <syusui.s@gmail.com>
2022-05-29 13:35:20 +0900

    split serializer

---
+++ src/serialize/sale.ts
@@ -0,0 +1,47 @@
+import Sale from '@/models/Sale';
+import SaleItem from '@/models/SaleItem';
+
+export type SaleSerialized = {
+  id: string;
+  soldAt: string;
+  items: SaleItemSerialized[];
+};
+
+export type SaleItemSerialized = {
+  productId: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+export const deserializeSales = (data: string | null): Sale[] | null => {
+  if (data === null) return null;
+
+  const parsed = JSON.parse(data) as SaleSerialized[];
+  return parsed.map((rawSale) => {
+    const items = rawSale.items.map((rawSaleItem) => {
+      return new SaleItem(
+        rawSaleItem.productId,
+        rawSaleItem.name,
+        rawSaleItem.price,
+        rawSaleItem.quantity,
+      );
+    });
+    return new Sale(rawSale.id, new Date(rawSale.soldAt), items);
+  });
+};
+
+export const serializeSales = (sales: Sale[]): string => {
+  const data: SaleSerialized[] = sales.map((sale) => ({
+    id: sale.id,
+    soldAt: sale.soldAt.toISOString(),
+    items: sale.items.map((saleItem) => ({
+      productId: saleItem.productId,
+      name: saleItem.name,
+      price: saleItem.price,
+      quantity: saleItem.quantity,
+    })),
+  }));
+
+  return JSON.stringify(data);
+};
--- src/useSales.ts
+++ src/useSales.ts
@@ -5,55 +5,11 @@ import Cart from '@/models/Cart';
 import Sale from '@/models/Sale';
 import SaleItem from '@/models/SaleItem';
 import useProducts from '@/useProducts';
+import { deserializeSales, serializeSales } from '@/serialize/sale';
 
 export type UseSales = {
   sales: Accessor<Sale[]>;
   register: (cart: Cart) => void;
-};
-
-type SaleSerialized = {
-  id: string;
-  soldAt: string;
-  items: SaleItemSerialized[];
-};
-
-type SaleItemSerialized = {
-  productId: string;
-  name: string;
-  price: number;
-  quantity: number;
-};
-
-const deserializeSales = (data: string | null): Sale[] | null => {
-  if (data === null) return null;
-
-  const parsed = JSON.parse(data) as SaleSerialized[];
-  return parsed.map((rawSale) => {
-    const items = rawSale.items.map((rawSaleItem) => {
-      return new SaleItem(
-        rawSaleItem.productId,
-        rawSaleItem.name,
-        rawSaleItem.price,
-        rawSaleItem.quantity,
-      );
-    });
-    return new Sale(rawSale.id, new Date(rawSale.soldAt), items);
-  });
-};
-
-const serializeSales = (sales: Sale[]): string => {
-  const data: SaleSerialized[] = sales.map((sale) => ({
-    id: sale.id,
-    soldAt: sale.soldAt.toISOString(),
-    items: sale.items.map((saleItem) => ({
-      productId: saleItem.productId,
-      name: saleItem.name,
-      price: saleItem.price,
-      quantity: saleItem.quantity,
-    })),
-  }));
-
-  return JSON.stringify(data);
 };
 
 const [sales, setSales] = createSignal<Sale[]>([]);
