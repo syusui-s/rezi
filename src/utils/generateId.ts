b51c78dc55f1fc8db80940b99232db39ae65a6ef
Shusui MOYATANI <syusui.s@gmail.com>
2022-08-13 18:25:28 +0900

    Use UUID for generateId

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,3 +1,19 @@
-const generateId = () => Math.random().toString();
+const generateId = () => {
+  if (window.crypto?.getRandomValues != null) {
+    const arr = crypto.getRandomValues(new Uint8Array(16));
+    // Version: 4
+    // eslint-disable-next-line no-bitwise
+    arr[6] = (arr[6] & 0x0f) | 0x40;
+    // Variant: 0b10 (RFC4122)
+    // eslint-disable-next-line no-bitwise
+    arr[8] = (arr[8] & 0x3f) | 0x80;
+    const binaryString = String.fromCharCode(...arr);
+    const b64String = btoa(binaryString);
+    const urlSafeString = b64String.replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '');
+    return urlSafeString;
+  }
+
+  return Math.random().toString();
+};
 
 export default generateId;
